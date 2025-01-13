0001bb91e4b12257b2c318ee58876ac791a0a6d1
BlackSoi1 <lixiaolongjack@gmail.com>
2025-01-13 16:19:02 +0800

    update o1 mini result

--- evaluation/run/run_eval.sh
+++ evaluation/run/run_eval.sh
@@ -1,3 +1,3 @@
 # jsonl_file="JSONL_FILE_FROM_INFERENCE"
-jsonl_file="/app/temp/0110_gpt-4o_final_output.jsonl"
+jsonl_file="/app/temp/0110_o1-mini_assistant_final_output.jsonl"
 python3 /app/src/evaluation.py --jsonl_file "$jsonl_file" 
